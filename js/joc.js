9237e2c3eb502fb5ed8ceae84e4cfea3f412b955
mapumi1819daw2 <marcileobcnapp@gmail.com>
2019-01-14 20:46:32 +0100

    Es mostra la fitxa seleccionada al tauler de joc

--- js/joc.js
+++ js/joc.js
@@ -3,6 +3,33 @@
 /* dada */
 
 
-function clicaFitxa(i){
-    
+
+
+function mouFitxa(i){
+    var mouFitxa = "[mouFitxa] ";
+    console.log(mouFitxa+ i);
+
+    var tauler = document.getElementById("divTauler");
+
+    /* Dibuixem la fitxa al tauler */
+    var fitxa = document.createElement("a");
+    var t = document.createTextNode("Fitxa "+ dada.fitxes[i]);
+    fitxa.appendChild(t);
+    fitxa.setAttribute("id", "t"+i);
+    tauler.appendChild(fitxa);
+
+
+    document.getElementById(i).setAttribute("hidden", "true");
+
 }
+
+/* Funció que respon al click de la fitxa */
+function clicaFitxa(i){
+    var clicFitxa = "[clicFitxa] ";
+    console.log(clicFitxa+ i);
+
+    mouFitxa(i);
+}
+
+
+
